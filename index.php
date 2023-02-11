3a31dbab099cab366a54612c46b42af2fbed9ed2
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2023-02-11 14:02:58 +0600

    first_php_code

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First Program In PHP</title>
+</head>
+<body>
+    <?php 
+        $name = "Hello World";
+        echo "<h1>Hello</h1>";
+    ?>
+
+    <h1><?php echo $name ?></h1>
+
+
+    
+</body>
+</html>
